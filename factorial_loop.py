72c4fdb886d529e402c7ad5e2128370d3ecee60b
azharhacks <azhar.abeid@strathmore.edu>
2025-04-25 11:19:06 +0300

    Add files via upload
    Get factorial of a number using for loop

---
+++ factorial_loop.py
@@ -0,0 +1,12 @@
+number=int(input("Enter any number to calculate its factorial"))
+result=0
+r=0
+
+for i in range(0,number,1):
+    result=number*number-1  #(5*4)
+    r=result+r   #to cumulate  the values
+
+
+
+
+print("the factorial is "+str(r))
